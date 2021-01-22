d785a5b712cc613704ec5c6b02407d4f947f0d42
James Mallette <jamespmallette@gmail.com>
2021-01-22 13:27:02 -0500

    remove eslintcache from git

--- src/components/GameSettings/SoundPicker.js
+++ src/components/GameSettings/SoundPicker.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { styled } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import ButtonGroup from '@material-ui/core/ButtonGroup';
